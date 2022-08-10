79f9917d619344cab41c21d1a040d62a03975dd6
unknown <francisco.rojas-m@labinfo.is.escuelaing.edu.co>
2022-08-10 09:39:55 -0500

    part one

--- PARALLELISM-JAVA_THREADS-INTRODUCTION_BLACKLISTSEARCH-master/src/main/java/edu/eci/arsw/threads/CountThread.java
+++ PARALLELISM-JAVA_THREADS-INTRODUCTION_BLACKLISTSEARCH-master/src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -4,11 +4,24 @@
  * and open the template in the editor.
  */
 package edu.eci.arsw.threads;
+import java.io.*;
+import java.lang.Thread;
 
 /**
  *
  * @author hcadavid
  */
-public class CountThread {
-    
+public class CountThread extends Thread {
+    public int A;
+    public int B;
+
+    public CountThread(int A, int B){
+        this.A = A;
+        this.B = B;
+    }
+    public void run(){
+        for(int i = A; i <= B; i++ ){
+            System.out.println(i);
+        }
+    }
 }
